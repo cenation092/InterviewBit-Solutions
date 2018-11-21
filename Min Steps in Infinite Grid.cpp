3a72a5564cd172a3ef142236419f3abcad027046
mohit rai <cenation092@gmail.com>
2018-11-21 05:52:14 +0530

    Create Min Steps in Infinite Grid.cpp

---
+++ Min Steps in Infinite Grid.cpp
@@ -0,0 +1,8 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int n = A.size();
+    int ans = 0;
+    for( int i = 0; i < n-1; i++ ){
+        ans += min( abs(A[i] - A[i+1]), abs(B[i] - B[i+1]) );
+    }
+    return ans;
+}
